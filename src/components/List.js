a632fb841a0a20229436447ac26035539e54e679
Nadia <nadiyaklokova@gmail.com>
2021-03-11 23:04:44 +0100

    List

---
+++ src/components/List.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function List(){
+    return(
+        <div className='lista'>
+            <h1 className='tituloLista'> Titulo de lista </h1>
+         </div>
+    )
+}
