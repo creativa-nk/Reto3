7349928ec58fd37616ff5c44094d853aa7494d48
Nadia <nadiyaklokova@gmail.com>
2021-03-12 22:58:26 +0100

    +data +store

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import List from './components/List';
-import Card from './components/Card'
+import Card from './components/Card';
+import store from './utils/store'
 import './App.css';
 
 function App() {
+  const [data,setData] = useState(store)
   return (
     <div className="App">
-      <List />
+      {data.listIds.map((listId)=>(
+       <List />
+      ))}
     </div>
   );
 }
