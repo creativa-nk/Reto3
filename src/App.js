824925457760231a104e0f63b1104cca28463b67
Nadia <nadiyaklokova@gmail.com>
2021-03-13 00:48:29 +0100

    +addMoreCard +uuid

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React, { useState } from 'react';
 import List from './components/List';
 import store from './utils/store';
+import StoreApi from './utils/storeApi';
+import {v4 as uuid} from 'uuid';
 import './App.css';
 
 function App() {
   const [data,setData] = useState(store);
+
+  const addMoreCard = (title, listId) => {
+   console.log(title, listId);
+   const newCardId = uuid();
+   console.log(newCardId);
+  }
+
   return (
-    <div className="App">
-      {data.listIds.map((listId)=>{
-        const list = data.lists[listId];
-        return(
-          <List list={list} key={listId}/>
-        )
-      })}
-    </div>
+    <StoreApi.Provider value = {{addMoreCard}}>
+      <div className="App">
+       {data.listIds.map((listId)=>{
+         const list = data.lists[listId];
+          return(
+           <List list={list} key={listId}/>
+          )
+        })}
+     </div>
+    </StoreApi.Provider>
   );
 }
 
