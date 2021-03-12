10de02d937f20ee983f035b929af20ad8b41f05f
Nadia <nadiyaklokova@gmail.com>
2021-03-13 00:17:47 +0100

    CardTitle,  target.value

--- src/components/InputCard.js
+++ src/components/InputCard.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 export default function InputCard({setOpen}){
+    const [cardTitle,setCardTitle] = useState('')
+    const handleOnChange = (e) =>{
+        setCardTitle(e.target.value);
+    }
     return(
         <div>
              <div>
-                <input className='inputCard' placeholder='añade descripcion de la tarea'/>
+                <input className='inputCard'
+                 placeholder='añade descripcion de la tarea'
+                  value={cardTitle}
+                   onChange={handleOnChange}/>
               </div>
               <div>
                   <button onClick={()=>setOpen(false)} >Añadir Tarea</button>
