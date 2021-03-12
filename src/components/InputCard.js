89abf04d18d09143f48bb673c42b860e7f92e929
Nadia <nadiyaklokova@gmail.com>
2021-03-12 22:58:59 +0100

    onClick

--- src/components/InputCard.js
+++ src/components/InputCard.js
@@ -8,7 +8,7 @@ export default function InputCard({setOpen}){
                 <input className='inputCard' placeholder='añade descripcion de la tarea'/>
               </div>
               <div>
-                  <button >Añadir Tarea</button>
+                  <button onClick={()=>setOpen(false)} >Añadir Tarea</button>
                   <button onClick={()=>setOpen(false)}>X</button>
               </div>
         </div>
