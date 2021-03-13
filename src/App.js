1a3f3b42379c6fa8dba44bee7a85b25c7751f8a1
Nadia <nadiyaklokova@gmail.com>
2021-03-14 00:25:57 +0100

    mover tarjetas dentro de la misma lista

--- src/App.js
+++ src/App.js
@@ -59,15 +59,53 @@ export default function App() {
   }
 
   const onDragEnd = (result) => {
-    const{ destinacion, source, draggableId} = result;
-    console.log('destination', destinacion, 'source',source, draggableId);
+    const{ destination, source, draggableId} = result;
+    console.log('destination', destination, 'source',source, draggableId);
 
-    if(!destinacion){ // si el destino es null( osea no es una lista >> devolver la tarea a su sitio)
+    if(!destination){ // si el destino es null( osea no es una lista >> devolver la tarea a su sitio)
       return;
     }
-  }; 
+    
+    if (type === 'list') {
+      const newListIds = data.listIds;
+      newListIds.splice(source.index, 1);
+      newListIds.splice(destination.index, 0, draggableId);
+      return;
+    }
 
+    const sourceList = data.lists[source.droppableId];
+    const destinationList = data.lists[destination.droppableId];
+    const draggingCard = sourceList.cards.filter(
+      (card) => card.id === draggableId
+    )[0];
 
+    if (source.droppableId === destination.droppableId) {
+      sourceList.cards.splice(source.index, 1);
+      destinationList.cards.splice(destination.index, 0, draggingCard);
+      const newSate = {
+        ...data,
+        lists: {
+          ...data.lists,
+          [sourceList.id]: destinationList,
+        },
+      };
+      setData(newSate);
+    } else {
+      sourceList.cards.splice(source.index, 1);
+      destinationList.cards.splice(destination.index, 0, draggingCard);
+
+      const newState = {
+        ...data,
+        lists: {
+          ...data.lists,
+          [sourceList.id]: sourceList,
+          [destinationList.id]: destinationList,
+        },
+      };
+      setData(newState);
+    }
+  };
+    
   return (
     <StoreApi.Provider value = {{addMoreCard, addMoreList}}>
       <DragDropContext onDragEnd={onDragEnd}>
