e5a7ad12607fbb01750a96a97a45f2891a181855
Nadia <nadiyaklokova@gmail.com>
2021-03-13 10:21:05 +0100

    card content por title

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ import React from 'react';
 export default function Card({card}){
     return(
         <div className='tarea'>
-            {card.content}
+            {card.title}
         </div>
     )
 }
