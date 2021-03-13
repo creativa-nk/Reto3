58e0b26c5e56574d235d6c0257f939bb42a235aa
Nadia <nadiyaklokova@gmail.com>
2021-03-13 11:33:01 +0100

    type list o card

--- src/components/InputConteiner.js
+++ src/components/InputConteiner.js
@@ -1,19 +1,21 @@
 import React,{useState} from 'react';
-import {Paper, Collapse} from '@material-ui/core';
+import { Paper, Typography, Collapse } from '@material-ui/core';
 import InputCard from './InputCard'
 
 
-export default function InputConteiner(listId){
+export default function InputConteiner(listId,type){
   const [open,setOpen] = useState(false)
     return(
-        <div >
+        <div className='inputConteiner'>
             <Collapse in={open}>
-              <InputCard setOpen={setOpen} listId={listId} />
+              <InputCard setOpen={setOpen} listId={listId} type={type} />   {/* que type es lista o card */}
             </Collapse>
             <Collapse in={!open}>
-              
-               <span onClick={()=>setOpen(!open)}> + Añadir tarea</span>
-              
+              <Paper>
+
+                  <span onClick={()=>setOpen(!open)}> + Añadir tarea</span>
+                
+              </Paper>
             </Collapse>
         </div>
     )
