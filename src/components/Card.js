fa17a09c339be6b271a0edd09542c4f04eeac1ef
Nadia <nadiyaklokova@gmail.com>
2021-03-17 14:59:49 +0100

    varios intentos de borrar tarea

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Draggable, Droppable } from 'react-beautiful-dnd';
+import store from '../utils/store'
+
 
 
 export default function Card({card,index}){
+  const [data,setData] = useState(store);
+
+
+   /* const [cards,setCards] = useState(store);
+     const DeleteCard = (listId) => {
+        setlist((prevState) =>
+          prevState.filter((todo, index) => index !== cardId)
+        );
+      };
+    console.log()  
+
+     const DeleteCard = (title,listId) => {
+
+        let { cards } = this.state;
+     
+        let newCards = [
+          ...card.slice(0, id),
+          ...card.slice(id + 1),
+        ]
+     
+        this.setState({ cards : newCards });
+     
+      } */
+    
+      const DeleteCard = (title, cardId) => {
+        const filtredData = setData.cards.filter(card => card.id !== cardId);
+        setData({cards:filtredData})
+        console.log(cardId)
+        console.log(filtredData)
+      };
+
+
     return(
         <Draggable draggableId={card.id} index={index}>
             {(provided)=>(
@@ -12,7 +46,7 @@ export default function Card({card,index}){
                      {...provided.draggableProps}
                 >
                      <div className='tarea'>{card.title}
-                        <div className='Xbtn'>X</div>
+                        <div className='Xbtn'  onClick={() => DeleteCard(cardId)} >X</div>
                      </div>
                 </div> 
             )}
